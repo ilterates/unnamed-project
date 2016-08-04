8421e8fd542b4c47aa86c70e92a45d78f3311bcc
ilterates <iltates@gmail.com>
2016-08-04 15:12:20 -0700

    working on posting

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,26 @@
 // public/js/app.js
-var app = angular.module('unnamed', ['ngRoute', 'appRoutes', 'MainCtrl', 'VideoCtrl', 'VideoService']);
+var app = angular.module('unnamed', ['ngRoute', 'appRoutes', 'MainCtrl', 'VideoCtrl','ContentCtrl', 'VideoService']);
+
+// $('#content-form').on("submit",function(e){
+//   e.preventDefault();
+//   var title = $("#title").val();
+//   var url = $("#url").val();
+//   var description = $("#description").val();
+//   console.log("button clicked");
+  // $.ajax({
+  //    url: "https://api.mlab.com/api/1/databases/unnamed/collections/my-coll?apiKey=Mqu4oGAuzrqCOpQOkUyZArFXtBol-o04",
+  // 	 data: JSON.stringify({
+  //      "title" : title,
+  //      "url" : url,
+  //      "description" : description
+  //    }),
+  // 	 type: "POST",
+  // 	 contentType: "application/json",
+  //    success: function(data){
+  //      console.log("success post");
+  //    },
+  //    error: function(xhr, status, err){
+  //      console.log(err);
+  //    }
+  //   } );
+// });
--- public/js/controllers/VideoCtrl.js
+++ public/js/controllers/VideoCtrl.js
@@ -1,6 +1,24 @@
 // public/js/controllers/VideoCtrl.js
-angular.module('VideoCtrl', []).controller('VideoController', function($scope) {
+angular.module('VideoCtrl', [])
+  .controller('VideoController', VideoController);
 
-    $scope.tagline = 'Video video Video!';
+  // $scope.tagline = 'Video video Video!';
 
-});
+VideoController.$inject = ['$http'];
+  function VideoController ($http) {
+    var vm = this;
+    var newContent = {};
+    vm.createVideo = function () {
+        $http({
+          method: 'POST',
+          url: 'https://api.mlab.com/api/1/databases/unnamed/collections/my-coll?apiKey=Mqu4oGAuzrqCOpQOkUyZArFXtBol-o04',
+          data: vm.newContent,
+        }).then(function successCallback(response) {
+          vm.newContent.push(response.data);
+          console.log("success");
+        }, function errorCallback(response) {
+          console.log('There was an error posting the data', response);
+        });
+
+    };
+  }
