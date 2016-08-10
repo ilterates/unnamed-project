2849d3a80afc754fe543b6e750858b7674a4e47c
PJC-1 <jumpzine@gmail.com>
2016-08-10 16:15:26 -0700

    cleaned up code

--- public/js/controllers/VideoCtrl.js
+++ public/js/controllers/VideoCtrl.js
@@ -7,26 +7,20 @@ angular.module('VideoCtrl', [])
     var vm = this;
     var newContent = {};
 
-    console.log("button clicked");
-      $http({
-        method: 'POST',
-        url: 'https://api.mlab.com/api/1/databases/unnamed/collections/content?apiKey=CFYgCuDerS1QjQ7LtxPzDLDR9OcxIyCA',
-        data: JSON.stringify({
-          title      : $('#title').val(),
-          link       : $('#link').val(),
-          description: $('#description').val()
-        }),
-        contentType: "application/json"
-      }).then(function successCallback(response) {
-        console.log(response.data);
-        console.log("success");
-      }, function errorCallback(response) {
-        console.log('There was an error posting the data', response);
-      });
-
-
-  };
-  var fake = function(){
-    console.log("testing the function call");
+    $http({
+      method: 'POST',
+      url: 'https://api.mlab.com/api/1/databases/unnamed/collections/content?apiKey=CFYgCuDerS1QjQ7LtxPzDLDR9OcxIyCA',
+      data: JSON.stringify({
+        title      : $('#title').val(),
+        link       : $('#link').val(),
+        description: $('#description').val()
+      }),
+      contentType: "application/json"
+    }).then(function successCallback(response) {
+      console.log(response.data);
+      console.log("success");
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
   };
 }]);
