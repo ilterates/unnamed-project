a619b86f065ae9a72aae9613756971100ba53a1c
ilterates <iltates@gmail.com>
2016-08-19 03:43:17 -0700

    injected  however video comes out empty

--- public/js/controllers/VideoCtrl.js
+++ public/js/controllers/VideoCtrl.js
@@ -1,7 +1,6 @@
 // public/js/controllers/VideoCtrl.js
 angular.module('VideoCtrl', [])
-  .controller('VideoController', ['$scope', '$http', '$injector', function($scope, $http, $injector) {
-
+  .controller('VideoController', ['$scope', '$http', '$sce', '$injector', function($scope, $http, $sce, $injector) {
   $scope.posting = function () {
     var vm = this;
 
@@ -31,9 +30,9 @@ angular.module('VideoCtrl', [])
     }).then(function getSuccessCallback(response) {
       $scope.lenght = Object.keys(response.data).reverse()[0];
       $scope.data = response.data;
-      console.log($scope.lenght);
-      console.log(response.data[$scope.lenght]);
-      // console.log(response.data);
+      console.log(response.data[$scope.lenght].link);
+      $scope.url = $sce.trustAsResourceUrl('response.data[$scope.lenght].link');
+
     }, function getErrorCallback(response) {
       console.log('There was an error getting the data', response);
     });
