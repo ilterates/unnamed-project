44ec38e392162e87d871d9bf8538616b9b7a6d3a
ilterates <iltates@gmail.com>
2016-08-20 00:21:22 -0700

    nodemodules ignored

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:9000
-app.listen(port);
+app.listen(process.env.PORT || port);
 
 // shoutout to the user
 console.log('Magic happens on port ' + port);
