2dc5f97f5547bab377a38d2a6ee5e1043a31d87a
PJC-1 <jumpzine@gmail.com>
2016-12-19 18:39:23 -0800

    changed password

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // url : 'mongodb://<dbPJC1>:<dbbasik1basik1>@ds042379.mlab.com:42379/unnamed'
+    // url : 'mongodb://<dbPJC1>:<dbpassword2>@ds042379.mlab.com:42379/unnamed'
     url : 'mongodb://localhost/unnamed'
     // url :  'mongodb://<dbuser>:<dbpassword>@ds042379.mlab.com:42379/unnamed'
 };
